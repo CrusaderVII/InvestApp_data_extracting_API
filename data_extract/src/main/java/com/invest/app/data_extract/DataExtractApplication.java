38c9d78ebb543f9e6e33f692c6495c9e6b080d76
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-08-09 00:28:08 +0300

    Creating repository

---
+++ data_extract/src/main/java/com/invest/app/data_extract/DataExtractApplication.java
@@ -0,0 +1,13 @@
+package com.invest.app.data_extract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExtractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataExtractApplication.class, args);
+	}
+
+}
---
+++ data_extract/src/test/java/com/invest/app/data_extract/DataExtractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invest.app.data_extract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataExtractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
