a15125bc6d48ceb772ae6c3a3b129c0e624b0966
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-10-07 22:50:05 +0300

    I added dockefile and also i enabled DiscoveryClient annotation.

--- data_extract/src/main/java/com/invest/app/data_extract/DataExtractApplication.java
+++ data_extract/src/main/java/com/invest/app/data_extract/DataExtractApplication.java
@@ -3,8 +3,10 @@ package com.invest.app.data_extract;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class )
+@EnableDiscoveryClient
 public class DataExtractApplication {
 
 	public static void main(String[] args) {
